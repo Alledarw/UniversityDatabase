e4088e86f3e80b86d7754937cb1322450311d720
FlorinelFrancisc <francisc_florinel@yahoo.com>
2023-12-07 14:37:25 +0100

    change id med code

--- tables.sql
+++ tables.sql
@@ -1,115 +1,115 @@
 CREATE TABLE institutions (
-  institution_id VARCHAR(10) PRIMARY KEY,
+  institution_code VARCHAR(10) PRIMARY KEY,
   institution_name VARCHAR(50)
 );
 
 CREATE TABLE programs (
-  program_id VARCHAR(10) PRIMARY KEY,
+  program_code VARCHAR(10) PRIMARY KEY,
   program_name VARCHAR(50)
 );
 
 CREATE TABLE institutions_programs_relations (
-  institution_id VARCHAR(10) REFERENCES institutions(institution_id),
-  program_id VARCHAR(10) REFERENCES programs(program_id),
-  PRIMARY KEY (institution_id, program_id)
+  institution_code VARCHAR(10) REFERENCES institutions(institution_code),
+  program_code VARCHAR(10) REFERENCES programs(program_code),
+  PRIMARY KEY (institution_code, program_code)
 );
 
 CREATE TABLE branches (
-  branch_id VARCHAR(10) PRIMARY KEY,
+  branch_code VARCHAR(10) PRIMARY KEY,
   name VARCHAR(50),
-  program_id VARCHAR(10) REFERENCES programs(program_id)
+  program_code VARCHAR(10) REFERENCES programs(program_code)
 );
 
 CREATE TABLE courses (
-  course_id VARCHAR(6) PRIMARY KEY,
+  course_code VARCHAR(6) PRIMARY KEY,
   name VARCHAR(50),
   credits INT,
   is_opening BOOLEAN,
   is_ended BOOLEAN,
-  institution_id VARCHAR(10) REFERENCES institutions(institution_id)
+  institution_id VARCHAR(10) REFERENCES institutions(institution_code)
 );
 
 CREATE TABLE limited_courses (
   capacity INT,
-  course_id VARCHAR(10) REFERENCES courses(course_id),
-  PRIMARY KEY (course_id)
+  course_code VARCHAR(10) REFERENCES courses(course_code),
+  PRIMARY KEY (course_code)
 );
 
 CREATE TABLE classifications (
-  classified_id VARCHAR(10) PRIMARY KEY,
+  classified_code VARCHAR(10) PRIMARY KEY,
   name VARCHAR(50)
 );
 
 CREATE TABLE classified (
-  classified_id VARCHAR(10) REFERENCES classifications(classified_id),
-  course_id VARCHAR(10) REFERENCES courses(course_id),
-  PRIMARY KEY (classified_id, course_id)
+  classified_code VARCHAR(10) REFERENCES classifications(classified_code),
+  course_code VARCHAR(10) REFERENCES courses(course_code),
+  PRIMARY KEY (classified_code, course_code)
 );
 
 CREATE TABLE students (
   idnr VARCHAR(10) PRIMARY KEY,
   first_name VARCHAR(100),
   last_name VARCHAR(100),
-  program_id VARCHAR(10) REFERENCES programs(program_id)
+  program_code VARCHAR(10) REFERENCES programs(program_code)
 );
 
 CREATE TABLE student_branches (
   student_idnr VARCHAR(10) REFERENCES students(idnr),
-  program_id VARCHAR(10) REFERENCES programs(program_id),
-  branch_id VARCHAR(10) REFERENCES branches(branch_id),
-  PRIMARY KEY (student_idnr, program_id, branch_id)
+  program_code VARCHAR(10) REFERENCES programs(program_code),
+  branch_code VARCHAR(10) REFERENCES branches(branch_code),
+  PRIMARY KEY (student_idnr, program_code, branch_code)
 );
 
 CREATE TABLE mandatory_program (
-  program_id VARCHAR(10) REFERENCES programs(program_id),
-  course_id VARCHAR(10) REFERENCES courses(course_id),
-  PRIMARY KEY (program_id, course_id)
+  program_code VARCHAR(10) REFERENCES programs(program_code),
+  course_code VARCHAR(10) REFERENCES courses(course_code),
+  PRIMARY KEY (program_code, course_code)
 );
 
 CREATE TABLE mandatory_branch (
-  course_id VARCHAR(10) REFERENCES courses(course_id),
-  program_id VARCHAR(10) REFERENCES programs(program_id),
-  branch_id VARCHAR(10) REFERENCES branches(branch_id),
-  PRIMARY KEY (course_id, program_id, branch_id)
+  course_code VARCHAR(10) REFERENCES courses(course_code),
+  program_code VARCHAR(10) REFERENCES programs(program_code),
+  branch_code VARCHAR(10) REFERENCES branches(branch_code),
+  PRIMARY KEY (course_code, program_code, branch_code)
 );
 
 CREATE TABLE recommended_branch (
-  course_id VARCHAR(10) REFERENCES courses(course_id),
-  program_id VARCHAR(10) REFERENCES programs(program_id),
-  branch_id VARCHAR(10) REFERENCES branches(branch_id),
-  PRIMARY KEY (course_id, program_id, branch_id)
+  course_code VARCHAR(10) REFERENCES courses(course_code),
+  program_code VARCHAR(10) REFERENCES programs(program_code),
+  branch_code VARCHAR(10) REFERENCES branches(branch_code),
+  PRIMARY KEY (course_code, program_code, branch_code)
 );
 
 CREATE TABLE registered (
-  course_id VARCHAR(10) REFERENCES courses(course_id),
+  course_code VARCHAR(10) REFERENCES courses(course_code),
   student_idnr VARCHAR(10) REFERENCES students(idnr),
-  PRIMARY KEY (course_id, student_idnr)
+  PRIMARY KEY (course_code, student_idnr)
 );
 
 CREATE TABLE taken (
-  taken_id SERIAL PRIMARY KEY,
-  course_id VARCHAR(10) REFERENCES courses(course_id),
+  taken_code SERIAL PRIMARY KEY,
+  course_code VARCHAR(10) REFERENCES courses(course_code),
   student_idnr VARCHAR(10) REFERENCES students(idnr),
   grade VARCHAR(1)
 );
 
 CREATE TABLE student_credit (
-  taken_id SERIAL REFERENCES taken(taken_id),
+  taken_code SERIAL REFERENCES taken(taken_code),
   score INT,
-  PRIMARY KEY (taken_id)
+  PRIMARY KEY (taken_code)
 );
 
 CREATE TABLE course_prerequisites (
-  course_id VARCHAR(10) REFERENCES courses(course_id),
-  prerequisites_course VARCHAR(6) REFERENCES courses(course_id),
-  PRIMARY KEY (course_id, prerequisites_course)
+  course_code VARCHAR(10) REFERENCES courses(course_code),
+  prerequisites_course VARCHAR(6) REFERENCES courses(course_code),
+  PRIMARY KEY (course_code, prerequisites_course)
 );
 
 CREATE TABLE waiting_list (
-  course_id VARCHAR(10) REFERENCES courses(course_id),
+  course_code VARCHAR(10) REFERENCES courses(course_code),
   student_idnr VARCHAR(10) REFERENCES students(idnr),
   created_date TIMESTAMP,
-  PRIMARY KEY (course_id, student_idnr)
+  PRIMARY KEY (course_code, student_idnr)
 );
 
 /* Test */
